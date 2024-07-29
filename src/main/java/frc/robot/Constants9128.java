02ec9714c3db21183a40366f0a0a40e9a3bae0a4
Squirrr <abdullah.khaled.542@k12.friscoisd.org>
2024-07-29 09:52:12 -0500

    7/29 - Added constants files for 9128 and 9752

--- src/main/java/frc/robot/Constants9128.java
+++ src/main/java/frc/robot/Constants9128.java
@@ -12,118 +12,12 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.lib.util.COTSTalonFXSwerveConstants;
 import frc.lib.util.SwerveModuleConstants;
-// import frc.robot.utils.ShooterConfig;
-// import frc.robot.utils.ShooterPreset;
 import frc.robot.utils.ShooterConfig;
 import frc.robot.utils.ShooterPreset;
 
-//FRC 9752
+//FRC 9128
 
-public final class Constants {
-    /* As per old code, given by Coach Abdurrehman vvv*/
-    public static final double stickDeadband = 0.1;
-    public static final int CANdleID = 1;
-    public static final int JoystickId = 0;
-    public static final int IncrementAnimButton = XboxController.Button.kRightBumper.value;
-    public static final int DecrementAnimButton = XboxController.Button.kLeftBumper.value;
-    public static final int BlockButton = XboxController.Button.kStart.value;
-    public static final int MaxBrightnessAngle = 90;
-    public static final int MidBrightnessAngle = 180;
-    public static final int ZeroBrightnessAngle = 270;
-    public static final int VbatButton = XboxController.Button.kA.value;
-    public static final int V5Button = XboxController.Button.kB.value;
-    public static final int CurrentButton = XboxController.Button.kX.value;
-    public static final int TemperatureButton = XboxController.Button.kY.value;
-    public static final boolean kIsTuningMode = true;
-    
-    public static final double headingPresetKp = 0.003*3.0;
-    public static final double headingPresetKd = 0.0001*3.0;
-    public static final double driveHeadingKp = 0.003*3.0;
-    public static final double driveHeadingKd = 0.0001*3.0;
-
-    public static final double translationalAutoP = 3.8;
-    public static final double rotationalAutoP = 6.25;
-
-    public static final class Universal {
-        public static final double voltageMin = 0;
-        public static final double voltageMax = 12;
-    }
-
-    // public static ShooterConfig getLookupTable(){
-    //     ShooterConfig shooterConfig = new ShooterConfig();
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(6.5, 2500, 2500, 1.9)); // Distance -> Bumper
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(10, 2500, 3500, 19.54)); // Distance -> Bumper
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(11.5, 2500, 4000, 36.27)); // Distance -> Bumper
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(12.6, 2500, 4500, 54.75)); // Distance -> Bumper
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(13.5, 2500, 4500, 75)); // Distance -> Bumper
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(14.5, 2500, 5500, 92)); // Distance -> Bumper
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(15.2, 3500, 5500, 110)); // Distance -> Bumper
-    //     shooterConfig.getShooterConfigs().add(new ShooterPreset(15.75, 3500, 6000, 128.4)); // Distance -> Bumper
-    //     return shooterConfig;
-    // }
-
-    public static ShooterConfig getLookupTable(){
-        ShooterConfig shooterConfig = new ShooterConfig();
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(10, 2500, 2500, 1.58)); // Distance -> Bumper
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(15.5, 2500, 3500, 20.50)); // Distance -> Bumper
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(18, 2500, 4000, 36.71)); // Distance -> Bumper
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(21, 2500, 4500, 54.84)); // Distance -> Bumper
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(22.8, 3000, 4500, 75.04)); // Distance -> Bumper
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(23, 3000, 5500, 92.12)); // Distance -> Bumper
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(24.5, 3500, 5500, 110.18)); // Distance -> Bumper
-        shooterConfig.getShooterConfigs().add(new ShooterPreset(25.9, 4000, 6000, 127.54)); // Distance -> Bumper
-        return shooterConfig;
-    }
-
-    public static final class LimelightConstants {
-        public static final String photonVisionName = "9752limelight";
-        public static final double limelightMountAngleDegrees = 22.3;
-        public static final double limelightLensHeightInches = 19;
-        public static final double goalHeightInches = 57;
-        public static final double cameraToSpeakerDistance = 44;
-        public static final double turnPID = 0.005;
-        public static final double heightToAprilTag = 38.625; //Dist from Ll to AprilTag
-        public static final double aprilTagToSpeakerHeight = 20;
-    }
-    /* As per old code, given by Coach Abdurrehman ^^^*/
-
-    public static double degreesToArmAngle(double degrees, double gearRatio) {
-        return degrees*56.1/90; //Returns rotations
-    }
-
-    public static final class IntakeConstants {
-        public static final int kIntakeMotorPort = 15;
-    }
-
-    public static final class ArmConstants {
-        public static final int kArmMotorPort = 16;
-        public static final double ampPos = 70;
-        public static final double subPos = 10;
-
-        public class ArmPIDConstants {
-            //ArmPID came from Rayyan b/c his worked more smoothly than mine
-            public static final double kG = 0.255;
-            public static final double kP = 1.5;
-            public static final double kI = 0;
-            public static final double kD = 0.2;
-        }
-    }
-
-    public static final class ShooterConstants {
-        public static final int kTransferMotorPort = 20; 
-        public static final int kLeftShooterMotorPort = 21; //SparkMAX Controller 
-        public static final int kRightShooterMotorPort = 20; //SparkMAX Controller
-        public static final int kServoPort = 9;
-        
-        public class ShooterPIDConstants {
-            public static final double kFF = 0.002;
-            public static final double kP = 0.002;
-            public static final double kI = 0.0;
-            public static final double kD = 0.001;
-        }
-    }
-    /*Swerve constants taken form 9752worlds*/
-
+public final class Constants9128 {
     public static final class Swerve {
         //9128
         public static final int pigeonID = 0;
@@ -267,16 +161,5 @@ public final class Constants {
         /* Constraint for the motion profilied robot angle controller */
         public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
                 kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-    }
-
-    public static class LoggerSubscription {
-        public static final String battery = "BATTERY";
-        public static final String error = "ERROR";
-        public static final String motor = "MOTOR";
-        public static boolean batteryIsSubbed = false;
-        public static boolean errorIsSubbed = false;
-        public static boolean motorIsSubbed = false;
-        public static final double lowBatteryVoltage = 11.5;
-        public static final double lowMotorVoltage = 12;
     }
 }
